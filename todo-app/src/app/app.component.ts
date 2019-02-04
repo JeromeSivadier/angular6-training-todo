0ff8f4722430c97529f5a31ce36cc29a66cd6368
Jerome Sivadier <jerome.sivadier@airbus.com>
2019-02-04 15:50:20 +0100

    TP1 - DataBinding

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+
+  variable = 0;
+  textContent = '';
+  allTextContents: string[] = [];
+
+  increment(): void {
+    this.variable++;
+  }
+
+  addToAllTextContents(value: string): void {
+    this.allTextContents.push(value);
+  }
+
+  sumCharacters(): number {
+    return this.allTextContents.reduce((acc, x) => acc + x.length, 0);
+  }
 }
